943f7fb662e90c962458c765b0acf4735cb01643
ChincholikarSneha <snehachincholikar04@gmail.com>
2024-05-22 17:03:33 +0530

    Add files via upload
    An interactive navigation menu that changes color when hovering over a navigation bar. It is created with the help of HTML,CSS and JS.

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 20) {
+            navbar.style.backgroundColor = '#001f3f';
+        } else {
+            navbar.style.backgroundColor = '#001f3f';
+        }
+    });
+});
